b129af698aabc787195b7c300c0beae56f1c227c
payel <payel.nandy2580@gmail.com>
2022-07-10 18:15:42 -0400

    roll the dice

--- roll.py
+++ roll.py
@@ -4,4 +4,33 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+import random
+# question1 = input("How many dices[0-3]?")
+question2 = input('Would you like to roll the dice[y/n]?')
 
+while question2 != 'n':
+    if question2 == 'y':
+        question1 = input('How many dices[1-3]?')
+        if question1 == '1':
+            die1 = random.randint(1,6)
+            print(die1)
+            question2 = input('Would you like to roll the dice[y/n]?')
+        elif question1 == '2':
+            die1 = random.randint(1,6)
+            die2 = random.randint(1,6)
+            print(die1,die2)
+            question2 = input('Would you like to roll the dice[y/n]?')
+        elif question1 == '3':
+            die1 = random.randint(1, 6)
+            die2 = random.randint(1, 6)
+            die3 = random.randint(1,6)
+            print(die1, die2,die3)
+            question2 = input('Would you like to roll the dice[y/n]?')
+        else:
+            print("Error")
+            question2 = input('Would you like to roll the dice[y/n]?')
+    else:
+        print('Invalid response. Please type "y" or "n".')
+        question2 = input('Would you like to roll the dice[y/n]?')
+
+print('Good-bye!')
